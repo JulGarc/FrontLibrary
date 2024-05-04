59f42d0a4486085ac19698ff514585998695e336
LangelU <62520537+LangelU@users.noreply.github.com>
2024-05-04 09:29:34 -0500

    ajustes varios

--- src/componentes/Registrarse.jsx
+++ src/componentes/Registrarse.jsx
@@ -50,10 +50,6 @@ const Registrarse = () => {
                     icon: 'success',
                     title: 'ÉXITO',
                     text: "Registro completado con éxito"
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        location.reload(); // Recarga la página
-                    }
                 })
             } else {
                 Swal.fire({
